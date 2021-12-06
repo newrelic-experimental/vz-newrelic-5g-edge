1bef97eda3cf47fed8eaa955b7f44c0b8668e644
Brad Schmitt <bschmitt@newrelic.com>
2021-12-06 12:25:33 -0600

    initial commit

---
+++ variables.tf
@@ -0,0 +1,220 @@
+variable "profile" {
+  type        = string
+  description = "AWS Credentials Profile to use"
+  default     = "default"
+}
+
+variable "region" {
+  type        = string
+  default     = "us-east-1"
+  description = "This is the AWS region."
+}
+
+variable "worker_key_name" {
+  type        = string
+  default     = "test_key"
+  description = "This is your EC2 key name."
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "wavelength"
+}
+
+# We use availability_zone_id for zone creation, and availability_zone for pod affinity
+variable "wavelength_zones" {
+  description = "This is the Wavelength Zone deployment metadata for your VPC, including Availability Zone ID, CIDR range per WLZ, NodePort, and number of worker nodes."
+  default = {
+    nyc = {
+      availability_zone    = "us-east-1-wl1-nyc-wlz-1",
+      availability_zone_id = "use1-wl1-nyc-wlz1",
+      worker_nodes         = 1,
+      cidr_block           = "10.0.10.0/24"
+      nodeport_offset      = 30100
+    },
+    bos = {
+      availability_zone    = "us-east-1-wl1-bos-wlz-1",
+      availability_zone_id = "use1-wl1-bos-wlz1",
+      worker_nodes         = 1,
+      cidr_block           = "10.0.11.0/24"
+      nodeport_offset      = 30200
+    },
+    # was = {
+    #   availability_zone = "us-east-1-wl1-was-wlz-1",
+    #   availability_zone_id = "use1-wl1-was-wlz1",
+    #   worker_nodes = 2,
+    #   cidr_block = "10.0.12.0/24"
+    #   nodeport_offset = 30300
+    # },
+    # atl = {
+    #   availability_zone = "us-east-1-wl1-atl-wlz-1",
+    #   availability_zone_id = "use1-wl1-atl-wlz1",
+    #   worker_nodes = 2,
+    #   cidr_block = "10.0.13.0/24"
+    #   nodeport_offset = 30400
+    # },
+    # mia = {
+    #   availability_zone = "us-east-1-wl1-mia-wlz-1",
+    #   availability_zone_id = "use1-wl1-mia-wlz1",
+    #   worker_nodes = 2,
+    #   cidr_block = "10.0.14.0/24"
+    #   nodeport_offset = 30500
+    # },
+  }
+}
+
+variable "availability_zones" {
+  description = "This is the Availability Zone (parent region) deployment metadata for your VPC, including Availability Zone and CIDR range for each AZ."
+  default = {
+    az1 = {
+      availability_zone_id = "use1-az1"
+      cidr_block           = "10.0.1.0/24"
+    },
+    az2 = {
+      availability_zone_id = "use1-az2"
+      cidr_block           = "10.0.2.0/24"
+    },
+    # use1-az3 had capacity issues at time of testing
+    # az3 = {
+    #   availability_zone_id = "use1-az3"
+    #   cidr_block = "10.0.23.0/24"
+    # },
+  }
+}
+
+# variable "availability_zone_1" {
+#   type        = string
+#   default     = "us-east-1a"
+#   description = "This is the first Availability Zone for the EKS control plane."
+# }
+
+# variable "availability_zone_2" {
+#   type        = string
+#   default     = "us-east-1b"
+#   description = "This is the second Availability Zone for the EKS control plane."
+# }
+
+variable "node_group_s3_bucket_url" {
+  type        = string
+  default     = "https://wavelengthtutorials.s3.amazonaws.com/wlz-eks-node-group.yaml"
+  description = "This is the S3 object URL of the EKS node group with auto-attached Carrier IPs."
+}
+
+variable "worker_volume_size" {
+  default     = 20
+  description = "This is the volume size (GB) of the EBS volumes for the EKS worker nodes."
+}
+
+variable "worker_instance_type" {
+  default     = "t3.xlarge"
+  description = "This is the EC2 instance type for the EKS worker nodes."
+}
+
+variable "require_imdsv2" {
+  default     = true
+  description = "This is a bool whether to use AWS Instance Metadata Service Version 2 (IMDSv2)."
+}
+
+# Create AMI Mapping for Wavelength Zone (EKS 1.21)
+variable "worker_image_id" {
+  type = map(string)
+  default = {
+    "us-east-1" = "ami-0193ebf9573ebc9f7"
+    "us-west-2" = "ami-0bb07d9c8d6ca41e8"
+  }
+  description = "This is the AMI for the EKS worker nodes with EKS version 1.21."
+}
+
+variable "worker_nodegroup_name" {
+  default     = "Wavelength-Node-Group"
+  description = "This is the name for the EKS worker nodes."
+
+}
+
+variable "nr_license_key" {
+  description = "New Relic License Key"
+  sensitive   = true
+}
+
+variable "pixie_api_key" {
+  description = "Pixie API Key found in New Relic Guided Install"
+  sensitive   = true
+}
+
+variable "pixie_deploy_key" {
+  description = "Pixie Deploy Key found in New Relic Guided Install"
+  sensitive   = true
+}
+
+# variable "domain" {
+#   default     = "lab.local"
+#   description = "This the Route53 domain name for your Confluent cluster."
+# }
+
+# variable "zoneid" {
+#   default     = "none"
+#   description = "This is the Route53 ZoneID of your Public Hosted Zone for your Confluent cluster."
+# }
+
+locals {
+  ports_in = [
+    443,
+    80,
+    22
+  ]
+  ports_out = [
+    0
+  ]
+}
+
+
+# variable "wavelength_zone" {
+#   type        = string
+#   default     = "us-east-1-wl1-nyc-wlz-1"
+#   description = "This is the Wavelength Zone to deploy the EKS node group."
+# }
+
+# variable "wavelength_zone_2" {
+#   type        = string
+#   default     = "us-east-1-wl1-bos-wlz-1"
+#   description = "This is the second Wavelength Zone to deploy the EKS node group."
+# }
+
+# variable "wavelength_zone_3" {
+#   type        = string
+#   default     = "us-east-1-wl1-was-wlz-1"
+#   description = "This is the third Wavelength Zone to deploy the EKS node group."
+# }
+
+# variable "wavelength_zone_4" {
+#   type        = string
+#   default     = "us-east-1-wl1-atl-wlz-1"
+#   description = "This is the fourth Wavelength Zone to deploy the EKS node group."
+# }
+
+# variable "wavelength_zone_5" {
+#   type        = string
+#   default     = "us-east-1-wl1-mia-wlz-1"
+#   description = "This is the fifth Wavelength Zone to deploy the EKS node group."
+# }
+
+# variable "wlz1" {
+#   default     = false
+#   description = "Bool to determine deployment of second Wavelength Zone node group."
+# }
+# variable "wlz2" {
+#   default     = false
+#   description = "Bool to determine deployment of second Wavelength Zone node group."
+# }
+# variable "wlz3" {
+#   default     = false
+#   description = "Bool to determine deployment of third Wavelength Zone node group."
+# }
+# variable "wlz4" {
+#   default     = false
+#   description = "Bool to determine deployment of fourth Wavelength Zone node group."
+# }
+# variable "wlz5" {
+#   default     = false
+#   description = "Bool to determine deployment of fifth Wavelength Zone node group."
+# }
