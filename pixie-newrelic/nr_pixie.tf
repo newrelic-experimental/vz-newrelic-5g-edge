a3dc3561e2e56433c52d8b881c5d1228fe1a0394
shawnj <shawnjaccts@gmail.com>
2021-12-20 09:38:00 -0800

    Add Helm Chart Vars

--- pixie-newrelic/nr_pixie.tf
+++ pixie-newrelic/nr_pixie.tf
@@ -27,16 +27,35 @@
 
 provider "helm" {
   kubernetes {
-    host                   = var.kubernetes_host_info["host"]
-    cluster_ca_certificate = base64decode(var.kubernetes_host_info["cluster_ca_certificate"])
-    token                  = var.kubernetes_host_info["token"]
+    #host                   = var.kubernetes_host_info["host"]
+    #cluster_ca_certificate = base64decode(var.kubernetes_host_info["cluster_ca_certificate"])
+    #token                  = var.kubernetes_host_info["token"]
+    config_path = var.kube_config_path
   }
 }
 
 provider "kubernetes" {
-  host                   = var.kubernetes_host_info["host"]
-  cluster_ca_certificate = base64decode(var.kubernetes_host_info["cluster_ca_certificate"])
-  token                  = var.kubernetes_host_info["token"]
+  #host                   = var.kubernetes_host_info["host"]
+  #cluster_ca_certificate = base64decode(var.kubernetes_host_info["cluster_ca_certificate"])
+  #token                  = var.kubernetes_host_info["token"]
+  config_path = var.kube_config_path
+}
+
+data "template_file" "pixie_values" {
+  template = file("${path.module}/templates/values.yaml")
+
+  vars = {
+    NR_BUNDLE_INFRA           = var.nr_bundle_infra
+    NR_BUNDLE_PROMETHEUS      = var.nr_bundle_prometheus
+    NR_BUNDLE_WEBHOOK         = var.nr_bundle_webhook
+    NR_BUNDLE_KSM             = var.nr_bundle_ksm
+    NR_BUNDLE_KUBE_EVENTS     = var.nr_bundle_kube_events
+    NR_BUNDLE_LOGGING         = var.nr_bundle_logging
+    NR_BUNDLE_PIXIE           = var.nr_bundle_pixie
+    NR_BUNDLE_PIXIE_CHART     = var.nr_bundle_pixie_chart
+    NR_BUNDLE_INFRA_OPERATOR  = var.nr_bundle_infra_operator
+    NR_BUNDLE_METRICS_ADAPTER = var.nr_bundle_metrics_adapter
+  }
 }
 
 # resource "null_resource" "check_nodes" {
@@ -60,6 +79,9 @@ provider "kubernetes" {
 #     environment = {
 #       #KUBECONFIG = module.eks_cluster.kubeconfig_filename
 #       KUBECONFIG = "/Users/bschmitt/newrelic/git-repos/vz-newrelic-5g-edge/wavelength-cluster/kubeconfig_wavelength-test"
+#       KUBESERVER = var.kubernetes_host_info["host"]
+#       KUBETOKEN  = var.kubernetes_host_info["token"]
+#       KUBECA     = base64decode(var.kubernetes_host_info["cluster_ca_certificate"])
 #     }
 #   }
 
@@ -75,6 +97,9 @@ provider "kubernetes" {
 #     environment = {
 #       #KUBECONFIG = module.eks_cluster.kubeconfig_filename
 #       KUBECONFIG = "../wavelength-cluster/kubeconfig_wavelength-test"
+#       KUBESERVER = var.kubernetes_host_info["host"]
+#       KUBETOKEN  = var.kubernetes_host_info["token"]
+#       KUBECA     = base64decode(var.kubernetes_host_info["cluster_ca_certificate"])
 #     }
 #   }
 #   depends_on = [
@@ -82,65 +107,63 @@ provider "kubernetes" {
 #   ]
 # }
 
-# resource "null_resource" "apply_pixie" {
-#   provisioner "local-exec" {
-#     command = "kubectl apply -f https://download.newrelic.com/install/kubernetes/pixie/latest/px.dev_viziers.yaml && kubectl apply -f https://download.newrelic.com/install/kubernetes/pixie/latest/olm_crd.yaml"
+resource "null_resource" "apply_pixie" {
+  provisioner "local-exec" {
+    command = "kubectl apply -f https://download.newrelic.com/install/kubernetes/pixie/latest/px.dev_viziers.yaml && kubectl apply -f https://download.newrelic.com/install/kubernetes/pixie/latest/olm_crd.yaml"
 
-#     environment = {
-#       #KUBECONFIG = module.eks_cluster.kubeconfig_filename
-#       KUBECONFIG = "../wavelength-cluster/kubeconfig_wavelength-test"
-#     }
-#   }
-#   # depends_on = [
-#   #   null_resource.patch_coredns
-#   # ]
-# }
+    environment = {
+      #KUBECONFIG = module.eks_cluster.kubeconfig_filename
+      #KUBECONFIG = "../wavelength-cluster/kubeconfig_wavelength-test
+      KUBECONFIG = var.kube_config_path
+      #KUBESERVER = var.kubernetes_host_info["host"]
+      #KUBETOKEN  = var.kubernetes_host_info["token"]
+      #KUBECA     = base64decode(var.kubernetes_host_info["cluster_ca_certificate"])
+    }
+  }
+  # depends_on = [
+  #   null_resource.patch_coredns
+  # ]
+}
 
 
-# resource "helm_release" "newrelic" {
-#   name             = "newrelic-bundle"
-#   repository       = "https://helm-charts.newrelic.com"
-#   chart            = "nri-bundle"
-#   namespace        = "newrelic"
-#   create_namespace = true
-#   depends_on = [
-#     kubernetes_manifest.customresourcedefinition_catalogsources_operators_coreos_com,
-#     kubernetes_manifest.customresourcedefinition_clusterserviceversions_operators_coreos_com,
-#     kubernetes_manifest.customresourcedefinition_installplans_operators_coreos_com,
-#     kubernetes_manifest.customresourcedefinition_operatorconditions_operators_coreos_com,
-#     kubernetes_manifest.customresourcedefinition_operatorgroups_operators_coreos_com,
-#     kubernetes_manifest.customresourcedefinition_operators_operators_coreos_com,
-#     kubernetes_manifest.customresourcedefinition_subscriptions_operators_coreos_com,
-#     kubernetes_manifest.customresourcedefinition_viziers_px_dev
-#   ]
+resource "helm_release" "newrelic" {
+  name             = "newrelic-bundle"
+  repository       = "https://helm-charts.newrelic.com"
+  chart            = "nri-bundle"
+  namespace        = "newrelic"
+  create_namespace = true
+  depends_on = [
+    null_resource.apply_pixie
+  ]
 
-#   values = [
-#     #file("${path.module}/values-test.yaml")
-#     file("${path.module}/values.yaml")
-#   ]
+  values = [
+    #file("${path.module}/values-test.yaml")
+    #file("${path.module}/values.yaml")
+    data.template_file.pixie_values.rendered
+  ]
 
-#   set {
-#     name  = "global.licenseKey"
-#     value = var.nr_license_key
-#   }
+  set {
+    name  = "global.licenseKey"
+    value = var.nr_license_key
+  }
 
-#   set {
-#     name  = "global.cluster"
-#     value = var.cluster_name
-#   }
+  set {
+    name  = "global.cluster"
+    value = var.cluster_name
+  }
 
-#   set {
-#     name  = "newrelic-pixie.apiKey"
-#     value = var.pixie_api_key
-#   }
+  set {
+    name  = "newrelic-pixie.apiKey"
+    value = var.pixie_api_key
+  }
 
-#   set {
-#     name  = "pixie-chart.deployKey"
-#     value = var.pixie_deploy_key
-#   }
+  set {
+    name  = "pixie-chart.deployKey"
+    value = var.pixie_deploy_key
+  }
 
-#   set {
-#     name  = "pixie-chart.clusterName"
-#     value = var.cluster_name
-#   }
-# }
+  set {
+    name  = "pixie-chart.clusterName"
+    value = var.cluster_name
+  }
+}
