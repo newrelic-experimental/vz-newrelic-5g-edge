859468c8472e045a0f24e7a5ffc63df8a279ab70
Brad Schmitt <bschmitt@newrelic.com>
2022-01-12 09:47:58 -0600

    initial commit

--- wavelength-cluster/outputs.tf
+++ wavelength-cluster/outputs.tf
@@ -1,13 +1,18 @@
-# output "broker_private_ip" {
-#   value       = data.aws_instance.edge_broker.*.private_ip
-#   description = "The Private IP address of the edge instances in the Wavelength Zone"
-# }
+output "broker_private_ip" {
+  value       = data.aws_instance.edge_broker.*.private_ip
+  description = "The Private IP address of the edge instances in the Wavelength Zone"
+}
 
-# output "broker_carrier_ip" {
-#   value       = data.aws_instance.edge_broker.*.carrier_ip
-#   description = "The Carrier IP address of the edge instances in the Wavelength Zone"
-# }
+output "broker_carrier_ip" {
+  value       = data.aws_instance.edge_broker.*.carrier_ip
+  description = "The Carrier IP address of the edge instances in the Wavelength Zone"
+}
 
-# output "subnets" {
-#   value = [for subnet in aws_subnet.region_subnets: subnet.id]
-# }
+output "subnets" {
+  value = [for subnet in aws_subnet.region_subnets: subnet.id]
+}
+
+output "cluster_name" {
+  value = data.aws_eks_cluster.cluster.id
+  description = "Cluster name"
+}
